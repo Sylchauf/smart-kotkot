d846a6f8dd84856f8a51efc8eccc8bfa4ab8114a
Sylvain Chauffaille <contact@sylchauf.net>
2022-03-02 19:21:35 +0100

    Fix config endpoint

--- pages/api/interface/config.ts
+++ pages/api/interface/config.ts
@@ -5,6 +5,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const config = getConfig();
 
   res.status(200).json({
+    ...config,
     locale: config.locale,
     theme: config.theme || "automatic",
   });
