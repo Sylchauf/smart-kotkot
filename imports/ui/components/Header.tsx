8944ea23e229f1b5a5901e3ac507e769c6db8c3c
Sylvain Chauffaille <contact@sylchauf.net>
2022-03-15 18:30:27 +0100

    Better responsive

--- imports/ui/components/Header.tsx
+++ imports/ui/components/Header.tsx
@@ -3,33 +3,58 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import MenuIcon from "@mui/icons-material/Menu";
+import useMobile from "../hooks/useMobile";
 
 import DeviceDisplayer from "./DeviceDisplayer";
 import Logo from "./Logo";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
+  const { isMobile } = useMobile();
+
+  const logo = <Logo onClick={() => navigate("/")} />;
+  const menu = (
+    <IconButton onClick={() => navigate("/settings")}>
+      <MenuIcon />
+    </IconButton>
+  );
+  const devices = <DeviceDisplayer />;
+
+  if (!isMobile)
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderBottom: `1px solid lightgray`,
+          paddingBottom: 4,
+          marginBottom: 16,
+        }}
+      >
+        {logo}
+
+        <div style={{ display: "flex", alignItems: "center" }}>
+          {devices}
+          {menu}
+        </div>
+      </div>
+    );
 
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        borderBottom: `1px solid lightgray`,
-        paddingBottom: 4,
-        marginBottom: 16,
-      }}
-    >
-      <Logo onClick={() => navigate("/")} />
-
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <DeviceDisplayer />
-
-        <IconButton onClick={() => navigate("/settings")}>
-          <MenuIcon />
-        </IconButton>
+    <div style={{ marginBottom: 16 }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        {logo}
+        {menu}
       </div>
+
+      {devices}
     </div>
   );
 };
